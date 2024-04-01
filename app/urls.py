094daa8ea51a9bcd1b2dbad0eb3f0c12b42c0b53
victorsedlak04@gmail.com <victorsedlak04@gmail.com>
2024-04-01 11:45:19 -0300

    Criação da API marcas

--- app/urls.py
+++ app/urls.py
@@ -9,11 +9,12 @@ from drf_spectacular.views import (
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from core.views import UserViewSet, AcessorioViewSet, CategoriaViewSet, CorViewSet
+from core.views import UserViewSet, AcessorioViewSet, CategoriaViewSet, CorViewSet, MarcaViewSet
 
 router = DefaultRouter()
 
 router.register(r"users", UserViewSet, basename="users")
+router.register(r"marcas", MarcaViewSet, basename="marcas")
 router.register(r"cores", CorViewSet, basename="cores")
 router.register(r"categorias", CategoriaViewSet, basename="categorias")
 router.register(r"acessorios", AcessorioViewSet, basename="acessorios")
